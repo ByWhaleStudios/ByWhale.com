99b6624d4ba8b4f4acc4f038b8c36516bd3d5fc3
Greg <gregt87@gmail.com>
2018-05-07 16:43:21 -0400

    added reason architecture

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
-cd /var/www/YOUR-DOMAIN.com && \
+cd $REMOTEAPPDIR && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
