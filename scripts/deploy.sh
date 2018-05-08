0401452b8339596611724a9e7212015357376800
Greg <gregt87@gmail.com>
2018-05-08 01:47:29 -0400

    ughh just not using env variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -x
 
 tar -czf package.tgz build && \
-scp package.tgz ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_APP_DIR} && \
+scp package.tgz deploy@167.99.0.204:/srv/www/bywhale.me/ && \
 ssh ${REMOTE_USER}@${REMOTE_HOST} 'bash -s' < ./scripts/untar.sh
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v9.10.0/bin
 
-cd ${REMOTEAPPDIR} && \
+cd /srv/www/bywhale.me/ && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
 npm install && \
