b2acf7d97e1ef24705244b8fdfb97945ce683b2a
Greg <gregt87@gmail.com>
2018-05-08 01:58:07 -0400

    strict host checking now set

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -x
 
 tar -czf package.tgz build && \
-scp package.tgz deploy@167.99.0.204:/srv/www/bywhale.me/ && \
-ssh ${REMOTE_USER}@${REMOTE_HOST} 'bash -s' < ./scripts/untar.sh
+scp -o "StrictHostKeyChecking no" package.tgz deploy@167.99.0.204:/srv/www/bywhale.me/ && \
+ssh -o "StrictHostKeyChecking no" ${REMOTE_USER}@${REMOTE_HOST} 'bash -s' < ./scripts/untar.sh
