44a6d7a69dec5a355ad5c95ac08db09b2512447e
Greg <gregt87@gmail.com>
2018-05-08 12:10:40 -0400

    changed to removign public folder

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,7 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v9.10.0/bin
 
-rm -rf /srv/www/bywhale.me/* && \
+rm -rf /srv/www/bywhale.me/public && \ #remove pubic folder
 cd /srv/www/bywhale.me/ && \
 tar zxvf package.tgz -C . && \
 mv build/* . # && \
