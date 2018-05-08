a899993a8e1af160c87c40dbe08e388b6a7d45a7
Greg <gregt87@gmail.com>
2018-05-08 11:05:21 -0400

    updated to do janky index only setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
-cp package.json build/package.json && \
+# cp package.json build/package.json && \
 tar -czf package.tgz build && \
 scp -o "StrictHostKeyChecking no" -i ~/.ssh/idrsa package.tgz deploy@167.99.0.204:/srv/www/bywhale.me && \
 ssh -o "StrictHostKeyChecking no" -i ~/.ssh/idrsa deploy@167.99.0.204 'bash -s' < ./scripts/untar.sh
