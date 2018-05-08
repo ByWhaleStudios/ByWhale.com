f2a3e7dcfc198717d4eb609fffb1a27522a94b17
Greg <gregt87@gmail.com>
2018-05-08 10:31:07 -0400

    fine added public key explicitly back

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -x
 
 tar -czf package.tgz build && \
-scp -o "StrictHostKeyChecking no" package.tgz deploy@167.99.0.204:/srv/www/bywhale.me && \
-ssh -o "StrictHostKeyChecking no" deploy@167.99.0.204 'bash -s' < ./scripts/untar.sh
+scp -o "StrictHostKeyChecking no" -i ~/.ssh/idrsa package.tgz deploy@167.99.0.204:/srv/www/bywhale.me && \
+ssh -o "StrictHostKeyChecking no" -i ~/.ssh/idrsa deploy@167.99.0.204 'bash -s' < ./scripts/untar.sh
