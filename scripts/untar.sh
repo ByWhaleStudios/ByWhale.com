d254742cfb4a5b5f017a11228d0701c69225103c
Greg <gregt87@gmail.com>
2018-05-08 12:03:29 -0400

    added removal step to build process

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,6 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v9.10.0/bin
 
+rm -rf /srv/www/bywhale.me/* && \
 cd /srv/www/bywhale.me/ && \
 tar zxvf package.tgz -C . && \
 mv build/* . # && \
