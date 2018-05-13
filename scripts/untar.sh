2162e2eb3926fa473ca7a75549911b14421fa953
Greg <gregt87@gmail.com>
2018-05-13 01:18:11 -0400

    added clean up portion to script

--- scripts/untar.sh
+++ scripts/untar.sh
@@ -6,8 +6,10 @@ export NVM_BIN=$HOME/.nvm/versions/node/v9.10.0/bin
 
 rm -rf /srv/www/bywhale.me/public && \ #remove pubic folder
 cd /srv/www/bywhale.me/ && \
-tar zxvf package.tgz -C . && \
-mv build/* . # && \
+rm -rf last_version && \
+rm -rf main && \
+tar zxvf package.tgz -C last_version && \
+mv last_version/build/* main/ # && \
 
 
 # mv build/package.json . && \
